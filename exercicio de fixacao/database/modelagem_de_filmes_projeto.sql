bdc583326c008ca39eb46bbf5204aa15ad2013b1
Nephyro <anderson.soare.4245@gmail.com>
2026-05-15 11:25:26 -0300

    iniciando tabela de idioma

--- exercicio de fixacao/database/modelagem_de_filmes_projeto.sql
+++ exercicio de fixacao/database/modelagem_de_filmes_projeto.sql
@@ -1,14 +1,64 @@
-show databases;
-
 create database db_fixacao_fime_projeto;
 
 use db_fixacao_fime_projeto;
+
+show tables;
 
 create table tbl_nacionalidade (
 	id 		int not null primary key auto_increment,
     pais_origem varchar(100) not null
 );
 
-show tables;
 
-desc tbl_nacionalidade;
+create table tbl_idioma (
+	id 		int not null primary key auto_increment,
+    nome 	varchar(45) not null,
+    sigla	varchar(8) not null
+);
+
+
+create table tbl_profissao (
+	id 			int not null primary key auto_increment,
+    nome 		varchar(45) not null,
+    cbo			varchar(8) not null,
+    descricao	varchar(100)
+);
+
+
+create table tbl_tipo_telefone (
+	id 			int not null primary key auto_increment,
+    tipo 		varchar(15) not null
+);
+
+
+create table tbl_tipo_email (
+	id 			int not null primary key auto_increment,
+    tipo 		varchar(20) not null
+);
+
+
+create table tbl_genero (
+	id 			int not null primary key auto_increment,
+    nome 		varchar(100) not null,
+    descricao	varchar(255),
+    sigla		varchar(8)
+);
+
+
+create table tbl_classificacao (
+	id 			int not null primary key auto_increment,
+    nome 		varchar(50) not null,
+    sigla		varchar(5) not null,
+    descricao	varchar(255) not null
+);
+
+
+create table tbl_produtora (
+	id 				int not null primary key auto_increment,
+    nome 			varchar(100) not null,
+    pais_origem		varchar(100) not null,
+    ano_fundacao	date,
+    site_oficial	varchar(255),
+    descricao		varchar(100),
+    logo_url		varchar(255)
+);
---
+++ exercicio de fixacao/model/DAO/idioma/idioma.js
@@ -0,0 +1,8 @@
+/********************************************************************************
+ * Objetivo: Arquivo responsável pelo CRUD no Banco de dados MySQL na tabela
+ *           Nacionalidade
+ * Data: 15/05/2026
+ * Autor: Anderson Ribeiro
+ * Versão: 1.0
+ ********************************************************************************/
+
